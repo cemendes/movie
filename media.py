4eb0351efedf3ef344a35874245f3cb18ff17383
Eduardo Mendes <eduardo.mendes@gmail.com>
2016-06-21 08:40:03 -0500

    feedback
    added comments.

--- media.py
+++ media.py
@@ -5,10 +5,12 @@ class Movie():
 
     VALID_RATINGS = ["G", "PG", "PG-13","R" ]
     def __init__(self,move_title, movie_storyline, poster_image, trailer_youtube):
+        '''This definition reads info about the movie as well as it trailer and poster image.'''
         self.title = move_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        '''This function calls the browser and plays the movie youtube trailer'''
         webbrowser.open(self.trailer_youtube_url)
